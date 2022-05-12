85350b0f95982ab5700885723a408b269e5c4a92
Thao-Nguyen Do <do.nguyen@web.de>
2022-05-12 22:48:11 +0200

    Fix test

--- test/db/test_db_helper.py
+++ test/db/test_db_helper.py
@@ -1,9 +1,13 @@
 import sqlite3
-
-from nozomi_cb_bot.db import db_helper
+from unittest import mock
 
 
 def test_sqlite_connect(mocker):
     mocker.patch("sqlite3.connect")
+    import sys
+
+    sys.modules["nozomi_cb_bot.config"] = mock.MagicMock()
+    from nozomi_cb_bot.db import db_helper
+
     db_helper.create_cb_db("test", 0, 0)
     sqlite3.connect.assert_called_once_with("test.db")
